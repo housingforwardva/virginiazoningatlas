6c3d1a9a9cceed0fe503b855fdd4e69ef3259b99
ericvmai <ericvmai@gmail.com>
2023-06-30 22:21:39 -0400

    Update data

--- gloucester.R
+++ gloucester.R
@@ -3,16 +3,13 @@ library(leaflet)
 library(sf)
 library(geojsonsf)
 
-district_bounds <- geojson_sf("data/gloucester_county_zoning_final.geojson")
+district_bounds <- st_read("data/GloucesterGloucesterCountyVA-ZoningDistricts.geojson")
 
 district_data <- read_csv("data/GloucesterGloucesterCountyVA-ZoningDistricts.csv")
-
-districts <- district_bounds |> 
-  left_join(district_data, by = "Abbreviated District Name")
 
 
 leaflet() |> 
   addProviderTiles("OpenStreetMap") |> 
-  addPolygons(data = districts, color = ~pal("Abbreivated District Name"))
+  addPolygons(data = district_bounds)
   
 
