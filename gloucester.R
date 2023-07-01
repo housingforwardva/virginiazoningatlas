aec6ac662d8f692dfe30322c6c53122b5d047e69
ericvmai <30978780+ericvmai@users.noreply.github.com>
2023-07-01 15:58:52 -0400

    Update gloucester

--- gloucester.R
+++ gloucester.R
@@ -1,15 +1,20 @@
 library(tidyverse)
+library(janitor)
 library(leaflet)
 library(sf)
-library(geojsonsf)
 
-district_bounds <- st_read("data/GloucesterGloucesterCountyVA-ZoningDistricts.geojson")
+district_bounds <- st_read("data/GloucesterGloucesterCountyVA-ZoningDistricts.geojson") |> 
+  clean_names()
 
 district_data <- read_csv("data/GloucesterGloucesterCountyVA-ZoningDistricts.csv")
 
+pal <- colorFactor(palette = c("#40C0C0", "#A29DD4", "#999999"),
+                   levels = c("Primarily Residential", "Mixed with Residential", "Nonresidential"))
 
-leaflet() |> 
-  addProviderTiles("OpenStreetMap") |> 
-  addPolygons(data = district_bounds)
+leaflet(district_bounds) |> 
+  addProviderTiles("CartoDB.Positron") |> 
+  addPolygons(color = "#ffffff", weight = 1, smoothFactor = 0.5,
+              fillColor = ~pal(type_of_zoning_district),
+              fillOpacity = 0.8)
   
 
