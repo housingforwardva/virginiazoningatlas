4d8e4840379da667f8224a5e15669c224829310f
ericvmai <ericvmai@gmail.com>
2023-09-26 10:48:22 -0400

    Revert "Revert "Merge branch 'main' of https://github.com/housingforwardva/virginiazoningatlas""
    This reverts commit 8ff725146eef098fb46c3ad254039340d4d4a223.

---
+++ r/hampton_roads/atlas/hr_adu.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(sf)
+library(leaflet)
+library(rmapshaper)
+
+hr_adu <- st_read("data/hr/hr_adu.geojson")
+
+hr_adu <- ms_simplify(hr_adu, keep = 0.3)
+
+
+pal <- colorFactor(palette =c("#8B85CA", "#40C0C0", "#011E41"),levels = c("R", "M", "X"))
+
+hr_adu_map <- leaflet(hr_adu) |> 
+  addProviderTiles(providers$CartoDB.Positron) |> 
+  addPolygons(weight = 1,
+              color = ~pal(type),
+              fillColor = ~pal(type),
+              fillOpacity = 0.65,
+              popup = paste0("Jurisdiction: ",
+                             hr_adu$jurisdiction,
+                             "<br>",
+                             "District: ",
+                             hr_adu$abbrvname)) |> 
+  addLegend(data = hr_adu,
+            labels = c("Primarily Residential", "Mixed with Residential", "Nonresidential"),
+            colors = c("#8B85CA", "#40C0C0", "#011E41"),
+            title = "Type of Zoning District") 
+
+
+hr_adu_map
+
+write_rds(hr_adu_map, "data/hr/hr_adu_map.rds")
--- r/hampton_roads/costar/hr_costar.R
+++ r/hampton_roads/costar/hr_costar.R
@@ -3,16 +3,35 @@ library(readxl)
 library(janitor)
 library(zoo)
 library(lubridate)
+library(fredr)
 
-hr_costar <- read_excel("data/hr/hr_costar_rent_market.xlsx") |> 
-  clean_names() |> 
-  filter(!str_detect(period, "QTD")) |> 
+# Data from CoStar was downloaded on 9.7.2023
+
+hr_costar <- read_excel("data/hr/hr_costar_rent_market.xlsx") |> # Load in xlsx spreadsheet from CoStar.
+  clean_names() |> # Clean up column names to be data friendly.
+  filter(!str_detect(period, "QTD")) |> # Remove Quarter-to-Date estimate. 
   mutate(year = substr(period, 1, 4)) |> 
   mutate(year = as.numeric(year)) |> 
-  mutate(period = as.Date(as.yearqtr(period, format = "%Y Q%q"), frac = 1)) |> 
+  mutate(period = as.yearqtr(period, format = "%Y Q%q"), frac = 1) |> 
   filter(year >= 2010) |> 
   select(period, year, bldgs = inventory_bldgs, units = inventory_units, rent = asking_rent_per_unit, 
          rent_growth = asking_rent_percent_growth_yr, vacancy_rate = vacancy_percent,
          uc_units = under_construction_units)
+
+cpi_ls <- fredr(
+  series_id = "CUUR0000SA0L2"
+) %>% 
+  select(date, value) %>% 
+  mutate(date = as.Date(date)) %>% 
+  mutate(value = as.numeric(value)) %>% 
+  mutate(year = year(date)) |> 
+  mutate(period = as.yearqtr(date, format = "%Y Q%q", frac = 1)) |> 
+  group_by(period) |> 
+  summarise(cpi = mean(value, na.rm = TRUE))
+
+hr_costar <- hr_costar |> 
+  left_join(cpi_ls, by = "period") |> 
+  transform(adj_price = (278.47967/cpi)*rent) 
+
 
 write_rds(hr_costar, "data/hr/hr_costar.rds")
