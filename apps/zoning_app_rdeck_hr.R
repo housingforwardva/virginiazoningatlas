275944376eb58e3421249f24ff6d776984a33d9d
Kyle Walker <kyle@walker-data.com>
2023-10-02 15:03:00 -0500

    styling, additional features in app

---
+++ apps/zoning_app_rdeck_hr.R
@@ -0,0 +1,247 @@
+library(shiny)
+library(rdeck)
+library(bslib)
+library(sf)
+library(htmltools)
+library(shinyWidgets)
+library(janitor)
+
+# zoning <- st_read("gloucester_zoning.geojson", quiet = TRUE) |> 
+#   clean_names() %>%
+#   dplyr::mutate(fill_color = dplyr::case_when(
+#     type_of_zoning_district == "Primarily Residential" ~ "#40C0C0",
+#     type_of_zoning_district == "Mixed with Residential" ~ "#A29DD4",
+#     type_of_zoning_district == "Nonresidential" ~ "#999999"
+#   )) %>%
+#   dplyr::select(type_of_zoning_district,
+#                 fill_color,
+#                 x1_family_treatment,
+#                 full_district_name,
+#                 overlay)
+
+zoning <- readr::read_rds("hr_vza_simple.rds") |>
+  st_cast("MULTIPOLYGON") %>%
+    dplyr::mutate(fill_color = dplyr::case_when(
+      type == "R" ~ "#40C0C0",
+      type == "M" ~ "#A29DD4",
+      type == "X" ~ "#999999",
+      TRUE ~ "#FFFFFF"
+    ), 
+    highlight_color = dplyr::case_when(
+      type == "R" ~ "#40C0C0ff",
+      type == "M" ~ "#A29DD4ff",
+      type == "X" ~ "#999999ff",
+      TRUE ~ "#FFFFFFff"
+    ))
+
+transit <- sf::st_read("data/hr_transit_all.geojson")
+
+type_choices <- c(
+  "Prohibited" = "prohibited",
+  "Allowed/Conditional" = "allowed",
+  "Public Hearing" = "hearing"
+)
+
+accessory_choices <- c(
+  "Allowed As of Right" = "allowed",
+  "Allowed Only After Public Hearing" = "hearing",
+  "Prohibited" = "prohibited"
+)
+
+
+# pal <- colorFactor(palette = c("#40C0C0", "#A29DD4", "#999999"),
+#                    levels = c("Primarily Residential", "Mixed with Residential", "Nonresidential"))
+
+ui <- page_fluid(
+  # Ensure the content takes the full width and height of the screen
+  tags$head(
+    tags$style(HTML("
+      /* Full-screen map styling */
+      #map {
+        background-color: #e5e5e5; /* Placeholder background color for the map */
+        position: absolute;
+      }
+      
+      /* Style for the floating menu */
+      .floating-panel {
+        z-index: 1000;
+        background-color: rgba(255, 255, 255, 0.5); /* Semi-transparent background */
+      }
+
+      /* Hover effect for the floating menu */
+      .floating-panel:hover {
+        background-color: rgba(255, 255, 255, 0.8); /* Less transparent background on hover */
+      }
+      
+      .shiny-input-container {
+          margin-bottom: 2px; /* Adjust this value as desired */
+      }
+      
+      .checkbox-opts .shiny-options-group {
+        margin-left: 10px;  /* Adjust as needed for indentation */
+        font-size: 0.8em;   /* Adjust as needed for font size */
+        margin-bottom: 2px;
+      }
+    "))
+  ),
+  fluidRow( 
+    style = "height: 100%", 
+    absolutePanel(
+      width = "300px",
+      class = "floating-panel",
+      top = 80, left = 20, 
+      img(src = "hfv_logo.png", style = "width: 150px;"),
+      p("This interactive map shows how outdated zoning laws make it hard to build diverse, affordable housing."),
+      p("Use checkboxes below to filter zones in the map. Click a town to see what % of its territory satisfies selected criteria.", style = "font-size: 80%;"),
+      p("Type of Zoning District", style = "color: gray; font-weight: bold; margin-bottom: 5px;"),
+      checkboxInput("single_family", "1-Family Housing"),
+      div(class = "checkbox-opts", 
+          conditionalPanel(
+            condition = "input.single_family == true",
+            checkboxGroupInput("single_family_options",
+                               label = NULL,
+                               choices = type_choices, 
+                               selected = type_choices)
+              )
+          ),
+      checkboxInput("two_family", "2-Family Housing"),
+      div(class = "checkbox-opts",
+          conditionalPanel(
+            condition = "input.two_family == true",
+            checkboxGroupInput("two_family_options",
+                               label = NULL,
+                               choices = type_choices, 
+                               selected = type_choices)
+          )
+          ),
+      checkboxInput("three_family", "3-Family Housing"),
+      div(class = "checkbox-opts", 
+          conditionalPanel(
+            condition = "input.three_family == true",
+            checkboxGroupInput("three_family_options",
+                               label = NULL,
+                               choices = type_choices, 
+                               selected = type_choices)
+          )
+        ),
+      checkboxInput("four_family", "4+ Family Housing"),
+      div(class = "checkbox-opts", 
+          conditionalPanel(
+            condition = "input.four_family == true",
+            checkboxGroupInput("four_family_options",
+                               label = NULL,
+                               choices = type_choices, 
+                               selected = type_choices)
+          )
+          ),
+      checkboxInput("accessory", "Accessory Dwelling Units"),
+      div(class = "checkbox-opts",
+          conditionalPanel(
+            condition = "input.accessory == true",
+            checkboxGroupInput("accessory_options",
+                               label = NULL,
+                               choices = accessory_choices, 
+                               selected = accessory_choices)
+          )
+        ),
+      hr(),
+      checkboxInput("transit_stops", "Show transit stops", value = FALSE),
+      chooseSliderSkin("Flat", color = "#40C0C0"),
+      sliderInput("opacity", "Zone opacity", min = 0, max = 100, 
+                  value = 80, ticks = FALSE)
+    ), 
+    mainPanel(
+      width = 12,
+      rdeckOutput("map", width = "95%", height = "95vh")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+
+  
+  output$map <- renderRdeck({
+    rdeck(map_style = mapbox_light(), theme = "light",
+          initial_view_state = view_state(center = c(-76.6814467, 37.4091757),
+                                          zoom = 9.5),
+          layer_selector = FALSE) %>%
+      add_polygon_layer(data = zoning, get_fill_color = fill_color, opacity = 0.8, 
+                        id = "zoning_layer", get_polygon = geometry, get_line_color = "#ffffff",
+                        get_line_width = 10, pickable = TRUE, auto_highlight = TRUE, highlight_color = highlight_color,
+                        tooltip = name, name = "Zoning") %>%
+      add_scatterplot_layer(data = transit, get_position = geometry, name = "Public Transit", 
+                            radius_min_pixels = 2, visible = FALSE, 
+                            id = "transit_layer", 
+                            get_fill_color = scale_color_category(service, palette = c("#E41A1C", "#377EB8", "#4DAF4A")))
+  })
+  
+  observeEvent(input$opacity, {
+    rdeck_proxy("map") %>%
+      update_polygon_layer(id = "zoning_layer",
+                  opacity = input$opacity / 100)
+  })
+  
+  
+  # Write a zoning filter that talks to all of the different options
+  zoning_filter <- reactive({
+    sf_opts <- input$single_family_options
+    f2_opts <- input$two_family_options
+    f3_opts <- input$three_family_options
+    f4_opts <- input$four_family_options
+    acc_opts <- input$accessory_options
+    
+    output <- zoning %>%
+      dplyr::filter(
+        family1_treatment %in% sf_opts,
+        family2_treatment %in% f2_opts,
+        family3_treatment %in% f3_opts,
+        family4_treatment %in% f4_opts,
+        accessory_treatment %in% acc_opts
+      )
+    
+    output
+    
+  })
+  
+  filtered_zoning <- reactive({
+
+    # if (input$single_family) {
+    #   output <- dplyr::filter(zoning, family1_treatment %in% input$single_family_options)
+    # }
+    
+    # if (input$two_family) {
+    #   output <- dplyr::filter(zoning, family2_treatment %in% input$two_family_options)
+    # }
+    # 
+    # if (input$three_family) {
+    #   output <- dplyr::filter(zoning, family3_treatment %in% input$three_family_options)
+    # }
+    # 
+    # if (input$four_family) {
+    #   output <- dplyr::filter(zoning, family4_treatment %in% input$four_family_options)
+    # }
+    
+    # return(output)
+  })
+  
+  observeEvent(zoning_filter(), {
+    rdeck_proxy("map") %>%
+      update_polygon_layer(data = zoning_filter(), id = "zoning_layer")
+  })
+  
+  observeEvent(input$transit_stops, {
+    proxy <- rdeck_proxy("map")
+    
+    if (input$transit_stops) {
+      proxy %>%
+        update_scatterplot_layer(id = "transit_layer", visible = TRUE)
+    } else {
+      proxy %>% 
+        update_scatterplot_layer(id = "transit_layer", visible = FALSE)
+    }
+  })
+  
+}
+
+shinyApp(ui = ui, server = server)
--- r/hampton_roads/atlas/hr_map.R
+++
@@ -1,6 +0,0 @@
-library(sf)
-library(leaflet)
-
-hr_region <- read_sf("data/hr/hr_region.geojson")
-
-write_rds(hr_region, "data/hr/hr_region.rds")
