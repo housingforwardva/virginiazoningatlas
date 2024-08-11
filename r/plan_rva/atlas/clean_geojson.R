81cabd552439ee1edabcf53cc93ca91d914b08ca
ericvmai <30978780+ericvmai@users.noreply.github.com>
2024-08-11 07:57:32 -0400

    planrva data cleaning

--- r/plan_rva/atlas/clean_geojson.R
+++ r/plan_rva/atlas/clean_geojson.R
@@ -8,12 +8,42 @@ library(sf)
 
 rva_punched <- geojson_sf("data/planrva/geo/rva_vza.geojson") 
 
+rva_custom <- geojson_sf("data/planrva/geo/rva_vza_customdata.geojson") |> 
+  janitor::clean_names() |> 
+  select(abbrvname = abbreviated_district_name, jurisdiction, county, customfielddata) |> 
+  st_drop_geometry()
+
+
+rva_custom_punched <- rva_punched |> 
+  left_join(rva_custom, by = c("abbrvname", "jurisdiction", "county"))
+
 rva_cfd <- read_csv("data/planrva/csv/planrva_cfd.csv") |> 
-  select(fid, id, customfielddata)
+  janitor::clean_names() |> 
+  select(abbrvname = abbreviated_district_name, jurisdiction, county, customfielddata) |>  
+  separate(customfielddata, into = c("min1", "min2", "min3", "min4", "max1", "max2", "max3", "max4", "sfd", "thonly", "adu_def"), sep = ',') |> 
+  mutate(across(.fns = ~str_remove_all(.x, '"')),
+         across(.fns = ~str_remove_all(.x, "\\{")),
+         across(.fns = ~str_remove_all(.x, "\\}"))) |> 
+  mutate(across(min1:adu_def,.fns = ~str_remove_all(.x, "41_116:")),
+         across(min1:adu_def,.fns = ~str_remove_all(.x, '41_117:')),
+         across(min1:adu_def,.fns = ~str_remove_all(.x, '41_118:')),
+         across(min1:adu_def,.fns = ~str_remove_all(.x, '41_119:')),
+         across(min1:adu_def,.fns = ~str_remove_all(.x, '41_125:')),
+         across(min1:adu_def,.fns = ~str_remove_all(.x, '41_126:')),
+         across(min1:adu_def,.fns = ~str_remove_all(.x, '41_127:')),
+         across(min1:adu_def,.fns = ~str_remove_all(.x, '41_128:')),
+         across(min1:adu_def,.fns = ~str_remove_all(.x, '45_121:')),
+         across(min1:adu_def,.fns = ~str_remove_all(.x, '45_122:')),
+         across(min1:adu_def, .fns = ~str_remove_all(.x,'50_135:'))) |> 
+  mutate(across(min1:adu_def,.fns = ~str_trim(.x))) |> 
+  mutate(across(min1:max4, .fns = ~as.numeric(.x)))
 
 rva_punched_cfd <- rva_punched |> 
-  left_join(rva_cfd,
-            by = c("fid", "id"))
+  inner_join(rva_cfd,
+            by = c("abbrvname", "jurisdiction"))
+
+
+head <- head(rva_punched_cfd, 5)
 
 # Data prep
 
@@ -22,57 +52,31 @@ rva_vza_data <- rva_punched_cfd |>
          family1_treatment, family2_treatment, family3_treatment, family4_treatment,
          accessory_treatment, plannedresidential_treatment,
          # accessory_owner_required, accessory_family_required, accessory_elderly_only, accessory_renter_prohibited,
-         acres, jurisdiction, county, customfielddata, tooltipnotes, expired) |> 
+         acres, jurisdiction, county = county.y, tooltipnotes, expired,
+         min1, min2, min3, min4, max1, max2, max3, max4, sfd, thonly, adu_def) |> 
   mutate(type = case_when( # Correct the labels for Type of Zoning District field.
-    type == "Non-Residential" ~ "Nonresidential",
-    type == "Residential" ~ "Primarily Residential",
-    type == "Mixed" ~ "Mixed with Residential",
-    type == "Overlay Not Affecting Use" ~ "Overlay Not Affecting Use"
-  )) |>
+    type == "X" ~ "Nonresidential",
+    type == "R" ~ "Primarily Residential",
+    type == "M" ~ "Mixed with Residential",
+    type == "O" ~ "Overlay Not Affecting Use")) |>
   mutate(jurisdiction = case_when( # Reformat town names.
     jurisdiction == "Hanover - Ashland" ~ "Ashland",
-    TRUE ~ jurisdiction
-  )) |> 
-  filter(overlay == FALSE) |> # Remove overlay districts from the analysis.
+    TRUE ~ jurisdiction)) |> 
+  filter(overlay == 0) |> # Remove overlay districts from the analysis.
   mutate(acres = as.numeric(acres))
+
+# write_csv(data, "data/planrva/csv/rva_vza.csv")
 
 # Parse the custom field data which is provided within a single column.
 
-rva_vza_clean_data <- rva_vza_data |> 
-  separate(customfielddata, into = c("min1", "min2", "min3", "min4", "max1", "max2", "max3", "max4", "sfd", "thonly"), sep = ',') |> 
-  mutate(across(min1:thonly, .fns = ~str_remove_all(.x, '"')),
-         across(min1:thonly,.fns = ~str_remove_all(.x, "\\{")),
-         across(min1:thonly,.fns = ~str_remove_all(.x, "\\}")),
-         across(min1:thonly,.fns = ~str_remove_all(.x, "41_116:")),
-         across(min1:thonly,.fns = ~str_remove_all(.x, '41_117:')),
-         across(min1:thonly,.fns = ~str_remove_all(.x, '41_118:')),
-         across(min1:thonly,.fns = ~str_remove_all(.x, '41_119:')),
-         across(min1:thonly,.fns = ~str_remove_all(.x, '41_125:')),
-         across(min1:thonly,.fns = ~str_remove_all(.x, '41_126:')),
-         across(min1:thonly,.fns = ~str_remove_all(.x, '41_127:')),
-         across(min1:thonly,.fns = ~str_remove_all(.x, '41_128:')),
-         across(min1:thonly,.fns = ~str_remove_all(.x, '45_121:')),
-         across(min1:thonly,.fns = ~str_remove_all(.x, '45_122:'))) |> 
-  mutate(across(min1:thonly,.fns = ~str_trim(.x))) 
 
+five <- head(rva_vza_clean_data, 20)
 
 rva_vza_nogeo <- rva_vza_clean_data |> 
   st_drop_geometry() 
 
-# Filter for Mercatus Center data quality check
-# 
 
-# mercatus_rva <- rva_vza_nogeo |> 
-#   filter(sfd == "f") |> 
-#   filter(family4_treatment == "prohibited") |> 
-#   filter(family3_treatment == "prohibited") |> 
-#   filter(family2_treatment == "prohibited") |> 
-#   filter(family1_treatment == "allowed")
-# 
-# write_csv(mercatus_rva, "data/rva/csv/mercatus_sf.csv")
-
-
-st_write(rva_vza_clean_data, "data/rva/geo/rva_vza_geo.geojson", append = FALSE)
-write_rds(rva_vza_clean_data, "data/rva/rds/rva_vza_geo.rds", compress = "none")
-write_rds(rva_vza_nogeo, "data/rva/rds/rva_vza_nogeo.rds", compress = "none")
+st_write(rva_vza_clean_data, "data/planrva/geo/rva_vza_geo.geojson", append = FALSE)
+write_rds(rva_vza_clean_data, "data/planrva/rds/rva_vza_geo.rds", compress = "none")
+write_rds(rva_vza_nogeo, "data/planrva/rds/rva_vza_nogeo.rds", compress = "none")
 
